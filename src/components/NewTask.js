b9e65fabd754d0a0bf623c763e2f2883ad9b6c63
Franco Alcaraz <franco_alcaraz25@hotmail.com>
2022-10-12 08:45:00 +1100

    change New Task to use Ref instead of State

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,19 +1,18 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 const NewTask = ({onAddNewTask}) => {
-    const [text, setText] = useState("");
+    const text = useRef();
 
     const handleAddButton = () => {
-        if (!text) return null
-        onAddNewTask(text);
-        setText("");
+        if (!text.current.value) return null
+        onAddNewTask(text.current.value);
+        text.current.value = "";
     }
 
     return (
         <div className="new-task">
-            <input className="new-task-text" id="new-task-text"
-                value={text}
-                onChange={ev => setText(ev.target.value)}
+            <input className="new-task-text" id="new-task-text" name="new-task-text"
+                ref={text}
                 placeholder="Write a new task..."
             />
             <button className="new-task-add" id="new-task-add"
